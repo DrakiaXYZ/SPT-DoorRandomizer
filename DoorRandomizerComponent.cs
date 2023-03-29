3b7dcf06ad269d1bdc6869317eda39c794757b0b
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-03-29 16:32:55 -0700

    Blah blah lazy patch random doors or something

---
+++ DoorRandomizerComponent.cs
@@ -0,0 +1,48 @@
+﻿using Comfort.Common;
+using EFT;
+using EFT.Interactive;
+using System;
+using UnityEngine;
+
+namespace DrakiaXYZ_DoorRandomizer
+{
+    internal class DoorRandomizerComponent : MonoBehaviour
+    {
+        public void Awake()
+        {
+            FindObjectsOfType<Door>().ExecuteForEach(door =>
+            {
+                if (door.DoorState == EDoorState.Open || door.DoorState == EDoorState.Shut)
+                {
+                    // Check if any doors have an open/closed state, but also a lock
+                    if (!string.IsNullOrEmpty(door.KeyId))
+                    {
+                        Console.WriteLine($"Door has open/close state, but has a key! {door.name}");
+                    }
+
+                    // Randomly open/close doors
+                    if (UnityEngine.Random.Range(0, 100) < 50)
+                    {
+                        door.DoorState = EDoorState.Open;
+                    }
+                    else
+                    {
+                        door.DoorState = EDoorState.Shut;
+                    }
+
+                    // Trigger "OnEnable" to make sure the properties are set correctly for interaction
+                    door.OnEnable();
+                }
+            });
+        }
+
+        public static void Enable()
+        {
+            if (Singleton<IBotGame>.Instantiated)
+            {
+                var gameWorld = Singleton<GameWorld>.Instance;
+                gameWorld.GetOrAddComponent<DoorRandomizerComponent>();
+            }
+        }
+    }
+}
---
+++ DoorRandomizerPlugin.cs
@@ -0,0 +1,36 @@
+﻿using Aki.Reflection.Patching;
+using BepInEx;
+using EFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrakiaXYZ_DoorRandomizer
+{
+    [BepInPlugin("xyz.drakia.doorrandomizer", "DrakiaXYZ-DoorRandomizer", "0.0.1")]
+    public class DoorRandomizerPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new DoorRandomizerPatch().Enable();
+        }
+    }
+
+    // I'm lazy, so we have patch class here
+    internal class DoorRandomizerPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(GameWorld).GetMethod(nameof(GameWorld.OnGameStarted));
+        }
+
+        [PatchPrefix]
+        public static void PatchPrefix()
+        {
+            DoorRandomizerComponent.Enable();
+        }
+    }
+}
