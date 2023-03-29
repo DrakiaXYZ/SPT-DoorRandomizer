80f987d64628c2ec96cb8f28162d1d8c02e3f564
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-03-29 15:46:52 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrakiaXYZ_DoorRandomizer
+{
+    public class Class1
+    {
+    }
+}
