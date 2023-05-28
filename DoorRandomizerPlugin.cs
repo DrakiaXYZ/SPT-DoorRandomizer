00fbccd9ac22485fbfbc3630ce3d23f1564a364f
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-05-27 17:18:06 -0700

    - Update version number

--- DoorRandomizerPlugin.cs
+++ DoorRandomizerPlugin.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace DrakiaXYZ.DoorRandomizer
 {
-    [BepInPlugin("xyz.drakia.doorrandomizer", "DrakiaXYZ-DoorRandomizer", "1.0.3")]
+    [BepInPlugin("xyz.drakia.doorrandomizer", "DrakiaXYZ-DoorRandomizer", "1.0.4")]
     public class DoorRandomizerPlugin : BaseUnityPlugin
     {
         public static int interactiveLayer;
